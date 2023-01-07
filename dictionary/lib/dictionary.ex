d80383e05c07cb8312f9b92908f6d50d922bb9ee
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-01-07 20:04:22 +0100

    Init hangman repo.

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,22 @@
+defmodule Dictionary do
+  @moduledoc ""
+"
+Documentation
+for `Dictionary`.
+""
+"
+
+@doc ""
+"
+Hello world.
+
+## Examples
+
+iex > Dictionary.hello(): world
+
+""
+"
+def hello do
+  :world
+end
+end
